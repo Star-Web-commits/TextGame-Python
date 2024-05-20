1eb424213134d3e56bc213b6bce832f93eda24ab
Star <85157670+Star-Web-commits@users.noreply.github.com>
2024-05-20 12:40:46 +1000

    added some code

--- Main.py
+++ Main.py
@@ -1,8 +1,30 @@
-#Otherside Picnic
-#Central hub(Contains: HP, )
+#Otherside Picnic inspired game
+from random import randint
+
+game_running = True
+game_results = []
+#Central hub(Contains: HP, Attack and heal??? )
+class Player:
+    def __init__(self, Name: str, Health:int, Attack: int, Heal: int, Status: str):
+        self.Name = Name
+        self.Health = Health
+        self.Attack = Attack
+        self.Heal = Heal
+        self.Status = Status
+
+class ForthKind:
+    def __init__(self, Name: str, Health: int, Attack: int, Status: str):
+        self.Name = Name
+        self.Health = Health
+        self.Attack = Attack
+        self.Status = Status
+
 
 #Enemies
 # Kunekune
+Kunekune = ForthKind()
+
+
 
 # Lady Hasshaku
 
